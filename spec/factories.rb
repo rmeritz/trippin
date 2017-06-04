62117dcc54925e41e7d8548b4fa1c7ff2a1c466d
Rebecca Meritz <rebecca@meritz.com>
2017-06-04 17:14:41 -0400

    A trip show multiple points

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,4 +2,11 @@ FactoryGirl.define do
   factory :trip do
     title "Spice Journey"
   end
+
+  factory :point do
+    trip
+    name "Moracco"
+    lat 31.791702
+    long (-7.092619999999999)
+  end
 end
--- spec/features/trip_spec.rb
+++ spec/features/trip_spec.rb
@@ -1,45 +1,54 @@
 require 'rails_helper'
 
-RSpec.feature "trip planning" do
+RSpec.feature "trip planning", :js do
   scenario "creating a trip" do
     visit new_trip_path
     fill_in I18n.t("helpers.label.trip.title"), with: "Florida"
     click_on I18n.t("helpers.submit.trip.create")
     expect(page).to have_text "Florida"
+    expect(page).to have_a_map_with_a_points
   end
 
-  scenario "adding a starting point to a trip", :js do
+  scenario "adding a starting point to a trip" do
     Geocoder::Lookup::Test.add_stub(
       "Orlando",
       [
         {
         address: "Orland, FL, USA",
-        latitude: 28.5383355,
-        longitude: -81.3792365
+        latitude: 28,
+        longitude: -81
     }])
 
     trip = FactoryGirl.create :trip
     visit trip_path trip
     fill_in I18n.t("helpers.label.point_creator.location"), with: "Orlando"
     click_on I18n.t("helpers.submit.point_creator.create")
-    expect(page).to have_a_map_with_a_point("Orlando, FL").at(28.538, -81.379)
+    expect(page).to have_a_map_with_a_points([-81, 28])
+  end
+
+  scenario "add a couple points to a trip" do
+    trip = FactoryGirl.create :trip
+    _idaho = FactoryGirl.create(
+      :point, trip: trip, lat: 44, long: -114
+    )
+    _utah = FactoryGirl.create(
+      :point, trip: trip, lat: 39, long: -111
+    )
+    visit trip_path trip
+    expect(page).to have_a_map_with_a_points([-114, 44], [-111, 39])
   end
 
   private
 
-  RSpec::Matchers.define :have_a_map_with_a_point do |name|
+  RSpec::Matchers.define :have_a_map_with_a_points do |*coordinates|
     match do |page|
-      map_present? && feature_at_coordinates?
-    end
-    chain :at do |lat, long|
-      @lat = lat
-      @long = long
+      map_present? && features_at_coordinates?(coordinates)
     end
     failure_message do
       if !@map_presence
         "The map canvas was missing."
       else
-        "Expected #{[@lat, @long]} Actual #{[@actual_lat, @actual_lon]}"
+        "Expected #{coordinates} Actual #{@actual_coordinates}"
       end
     end
 
@@ -47,10 +56,17 @@ RSpec.feature "trip planning" do
       @map_presence = find("#map").has_selector?("canvas")
     end
 
-    def feature_at_coordinates?
-       @actual_lon, @actual_lat = page.driver.evaluate_script(
-         "ol.proj.toLonLat(map.getLayers().item(1).getSource().getFeatures()[0].getGeometry().getCoordinates())").map {|coord| coord.round(3) }
-      [@lat, @long] == [@actual_lat, @actual_lon]
+    def features_at_coordinates?(coordinates)
+      @actual_coordinates = actual_coordinates.map do |coords|
+        coords.map {|coord| coord.round }
+      end
+      Set.new(coordinates) == Set.new(@actual_coordinates)
+    end
+
+    def actual_coordinates
+      page.driver.evaluate_script(
+         "map.getLayers().item(1).getSource().getFeatures().map(function(feature) {return ol.proj.toLonLat(feature.getGeometry().getCoordinates())})"
+      )
     end
   end
 end
