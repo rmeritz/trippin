5fa43455494c4ddd2a957e174ecb9896db1f688f
Rebecca Meritz <rebecca@meritz.com>
2017-06-04 14:40:21 -0400

    Test shows marker is in Flordia

--- spec/features/trip_spec.rb
+++ spec/features/trip_spec.rb
@@ -13,16 +13,35 @@ RSpec.feature "trip planning" do
     visit trip_path trip
     fill_in I18n.t("helpers.label.point_creator.location"), with: "Orlando"
     click_on I18n.t("helpers.submit.point_creator.create")
-    expect(page).to have_a_map_with_a_point "Orlando, FL"
+    expect(page).to have_a_map_with_a_point("Orlando, FL").at(28.538, -81.379)
   end
 
   private
 
   RSpec::Matchers.define :have_a_map_with_a_point do |name|
     match do |page|
-      find("#map").has_selector?("canvas")
-      # see that this is a marker on the point
-      # label on the point (or click point to see label)
+      map_present? && feature_at_coordinates?
+    end
+    chain :at do |lat, long|
+      @lat = lat
+      @long = long
+    end
+    failure_message do
+      if !@map_presence
+        "The map canvas was missing."
+      else
+        "Expected #{[@lat, @long]} Actual #{[@actual_lat, @actual_lon]}"
+      end
+    end
+
+    def map_present?
+      @map_presence = find("#map").has_selector?("canvas")
+    end
+
+    def feature_at_coordinates?
+       @actual_lon, @actual_lat = page.driver.evaluate_script(
+         "ol.proj.toLonLat(map.getLayers().item(1).getSource().getFeatures()[0].getGeometry().getCoordinates())").map {|coord| coord.round(3) }
+      [@lat, @long] == [@actual_lat, @actual_lon]
     end
   end
 end
